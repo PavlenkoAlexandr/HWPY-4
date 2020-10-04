3e734c74a31154f96ceec3c83c1e48ff26a4f23d
Alexandr Pavlenko <alcohollex2@gmail.com>
2020-10-04 10:00:32 +0300

    top_10_news.py edit

--- top_10_news.py
+++ top_10_news.py
@@ -1,4 +1,14 @@
 import json
+import xml.etree.ElementTree as ET
+
+def top_10_words(lst):
+    words_list = lst.split()
+    words_dict = dict()
+    for word in words_list:
+        if len(word) > 6:
+            words_dict[word] = words_list.count(word)
+    descriptions_sorted = sorted(words_dict.items(), key=lambda x: x[1], reverse=True)
+    return descriptions_sorted[:10]
 
 with open('newsafr.json', encoding='utf-8') as f:
     json_data = json.load(f)
@@ -11,18 +21,9 @@ for description in descriptions:
     json_descriptions.replace('[', '')
     json_descriptions.replace(']', '')
 
-def top_10_words(str_):
-    words_list = str_.split()
-    words_dict = dict()
-    for word in words_list:
-        if len(word) > 6:
-            words_dict[word] = words_list.count(word)
-    descriptions_sorted = sorted(words_dict.items(), key=lambda x: x[1], reverse=True)
-    return descriptions_sorted[:10]
-
 print(top_10_words(json_descriptions))
 
-import xml.etree.ElementTree as ET
+
 parser = ET.XMLParser(encoding='utf-8')
 tree = ET.parse('newsafr.xml', parser)
 root = tree.getroot()
@@ -33,4 +34,3 @@ for item in items:
     xml_descriptions += item.find('description').text.lower()
 
 print(top_10_words(xml_descriptions))
-
