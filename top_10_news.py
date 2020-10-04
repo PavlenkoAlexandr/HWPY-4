e82b91f2275e665078b544ca3efe50a90ca6e087
Alexandr Pavlenko <alcohollex2@gmail.com>
2020-10-04 09:52:53 +0300

    top_10_news.py

---
+++ top_10_news.py
@@ -0,0 +1,36 @@
+import json
+
+with open('newsafr.json', encoding='utf-8') as f:
+    json_data = json.load(f)
+
+descriptions = json_data['rss']['channel']['items']
+json_descriptions = str()
+
+for description in descriptions:
+    json_descriptions += str(description['description']).lower()
+    json_descriptions.replace('[', '')
+    json_descriptions.replace(']', '')
+
+def top_10_words(str_):
+    words_list = str_.split()
+    words_dict = dict()
+    for word in words_list:
+        if len(word) > 6:
+            words_dict[word] = words_list.count(word)
+    descriptions_sorted = sorted(words_dict.items(), key=lambda x: x[1], reverse=True)
+    return descriptions_sorted[:10]
+
+print(top_10_words(json_descriptions))
+
+import xml.etree.ElementTree as ET
+parser = ET.XMLParser(encoding='utf-8')
+tree = ET.parse('newsafr.xml', parser)
+root = tree.getroot()
+
+items = root.findall('channel/item')
+xml_descriptions = str()
+for item in items:
+    xml_descriptions += item.find('description').text.lower()
+
+print(top_10_words(xml_descriptions))
+
